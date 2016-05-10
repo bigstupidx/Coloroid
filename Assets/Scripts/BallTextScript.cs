3d3020c9eac1cffe98d3636be372a0de97dc3a77
msukenik <m45sukenik@gmail.com>
2016-05-10 22:08:43 +0200

    ffff

---
+++ Assets/Scripts/BallTextScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTextScript : MonoBehaviour {
+	private TextMesh textMesh;
+	public int ballNumber = 1;
+
+	// Use this for initialization
+	void Start () {
+		try {
+			textMesh = GetComponentInChildren<TextMesh>();
+			textMesh.text = ballNumber.ToString();
+		} catch {
+			Debug.Log("text == null");
+		}
+	}
+}
